6547651ed392f94d41e1523b0b9e907c8e0c12e7
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-02-17 21:01:44 +0100

    built checkout tabs

---
+++ UI/js/checkout.js
@@ -0,0 +1,17 @@
+// select tab buttons
+const tabButtons = document.querySelectorAll(".tablink");
+const tabPanels = document.querySelectorAll(".tabcontent");
+
+const showPanel = (panelIndex, colorCode) => {
+  tabButtons.forEach(btn => {
+    btn.style.backgroundColor = "";
+    btn.style.color = "";
+  });
+  tabButtons[panelIndex].style.backgroundColor = colorCode;
+  tabButtons[panelIndex].style.color = "white";
+
+  tabPanels.forEach(btn => {
+    btn.style.display = "none";
+  });
+  tabPanels[panelIndex].style.display = "block";
+};
