5662d0845ffa19b32a5f2c7f03d026988ed497a6
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-02-17 02:04:07 +0100

    created meal model

---
+++ api/models/meal.model.js
@@ -0,0 +1,12 @@
+class Meal {
+  constructor() {
+    this.id = null;
+    this.name = null;
+    this.description = null;
+    this.size = null;
+    this.price = null;
+    this.caterer_id = null;
+  }
+}
+
+export default Meal;
