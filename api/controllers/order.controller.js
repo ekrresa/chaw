c89293013e7af227e8765104a17299691c80683c
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-02-17 15:47:59 +0100

    implemented getsingleorder method in order controller

---
+++ api/controllers/order.controller.js
@@ -0,0 +1,33 @@
+import orderService from '../services/order.services';
+
+const orderController = {
+  getOrders(req, res) {
+    const orders = orderService.getOrders();
+    return res
+      .json({
+        status: 'success',
+        data: orders
+      })
+      .status(200);
+  },
+
+  getSingleOrder(req, res) {
+    const { id } = req.params;
+    const order = orderService.getSingleOrder(id);
+
+    if (!order) {
+      return res
+        .status(404)
+        .send({ status: 'error', message: 'order does not exist' });
+    }
+
+    return res
+      .json({
+        status: 'success',
+        data: order
+      })
+      .status(200);
+  }
+};
+
+export default orderController;
